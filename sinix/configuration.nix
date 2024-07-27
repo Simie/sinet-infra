1ffb2f2c1c95511ac45ff02be8ba6d0f60037733
Simon Moles <me@simonmoles.com>
2024-07-27 15:34:21 +0100

    Switch to 'fish' shell

--- sinix/configuration.nix
+++ sinix/configuration.nix
@@ -59,9 +59,18 @@
 # Users
 ##########
 
-  # Enable ZSH as the default shell
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;  
+  # Enable fish as the default shell when bash starts in interactive mode (see https://nixos.wiki/wiki/Fish)
+  programs.fish.enable = true;
+  programs.bash = {
+    interactiveShellInit = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+
   users.groups.ssh  = {}; # Define a group for users allowed login remotely via SSH
 
   # Setup Users
